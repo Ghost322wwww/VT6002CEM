bba20bb68d5fb03e0ec83fa7d898002a79fd9039
Ghost322wwww <kwokc289@yahoo.com>
2025-01-23 19:36:16 +0800

    add imageButton

--- newproject/app/src/main/java/com/example/newproject/AiChatRoomActivity.java
+++ newproject/app/src/main/java/com/example/newproject/AiChatRoomActivity.java
@@ -2,10 +2,12 @@ package com.example.newproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AiChatRoomActivity extends AppCompatActivity {
+    ImageButton btn_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
