df89f05e42e770501cde4c91ed2ec60655a66c00
Ghost322wwww <kwokc289@yahoo.com>
2025-01-23 19:39:58 +0800

    add carview
    give the user send msg

--- newproject/app/src/main/java/com/example/newproject/AiChatRoomActivity.java
+++ newproject/app/src/main/java/com/example/newproject/AiChatRoomActivity.java
@@ -6,10 +6,12 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class AiChatRoomActivity extends AppCompatActivity {
     ImageButton btn_back;
     EditText text_msg;
+    CardView btn_sendMsg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
