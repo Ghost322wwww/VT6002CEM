9fb3d4492c10f59ceb7cf2483a7a390c96bb1d03
Ghost322wwww <kwokc289@yahoo.com>
2025-01-24 16:34:13 +0800

    Update mapPageActivity.java

--- newproject/app/src/main/java/com/example/newproject/mapPageActivity.java
+++ newproject/app/src/main/java/com/example/newproject/mapPageActivity.java
@@ -78,7 +78,7 @@ public class mapPageActivity extends AppCompatActivity implements OnMapReadyCall
                                 new CameraPosition.Builder()
                                         .target(gMap.getCameraPosition().target)
                                         .zoom(gMap.getCameraPosition().zoom)
-                                        .bearing(currentBearing + rotation * 2)
+                                        .bearing(currentBearing + rotation * 10)
                                         .build()
                         ));
                     }
