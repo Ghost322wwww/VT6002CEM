7e4d92ffd0d53c3fa246e3b704b0728b158d06b2
Ghost322wwww <kwokc289@yahoo.com>
2025-01-28 23:26:43 +0800

    new the camera function

--- newproject/app/src/main/java/com/example/newproject/ProfileActivity.java
+++ newproject/app/src/main/java/com/example/newproject/ProfileActivity.java
@@ -1,8 +1,12 @@
 package com.example.newproject;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
@@ -26,9 +31,13 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
+
 public class ProfileActivity extends AppCompatActivity {
 
     private static final int PICK_IMAGE = 123;
+    private static final int CAMERA_REQUEST_CODE = 124;
+
     TextView profileName, profileEmail, profilePhone;
     TextView titleName;
     ImageView profileImg;
@@ -60,12 +69,30 @@ public class ProfileActivity extends AppCompatActivity {
         profileImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+                CharSequence options[] = new CharSequence[] {"Take Photo", "Choose from Gallery"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(ProfileActivity.this);
+                builder.setTitle("Update Profile Picture");
+
+                builder.setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (which == 0) {
+                            // 打開相機
+                            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+                                startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+                            }
+                        } else if (which == 1) {
+                            // 從相簿選擇
+                            Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            startActivityForResult(galleryIntent, PICK_IMAGE);
+                        }
+                    }
+                });
+                builder.show();
             }
         });
+
 
         titleName.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -150,29 +177,39 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == PICK_IMAGE) {
-            if(resultCode == RESULT_OK) {
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == PICK_IMAGE && data != null) {
+                // 選擇相片
                 Uri imageUri = data.getData();
-                StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("user_images");
-                StorageReference imageReference = storageReference.child(imageUri.getLastPathSegment());
-                imageReference.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                uploadImageToFirebase(imageUri);
+            } else if (requestCode == CAMERA_REQUEST_CODE && data != null) {
+                // 拍攝相片
+                Bitmap photo = (Bitmap) data.getExtras().get("data");
+                profileImg.setImageBitmap(photo);
+
+                // 轉換 Bitmap 為 Uri
+                Uri tempUri = getImageUri(getApplicationContext(), photo);
+                uploadImageToFirebase(tempUri);
+            }
+        }
+    }
+
+    private void uploadImageToFirebase(Uri imageUri) {
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("user_images");
+        StorageReference imageReference = storageReference.child(imageUri.getLastPathSegment());
+
+        imageReference.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                imageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                     @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        imageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                            @Override
-                            public void onSuccess(Uri uri) {
-                                String imageURL = uri.toString();
-                                FirebaseUser firebaseUser = auth.getCurrentUser();
-                                DatabaseReference userReference = FirebaseDatabase.getInstance().getReference().child("user").child(firebaseUser.getUid());
-                                userReference.child("userImg").setValue(imageURL);
-                                Glide.with(ProfileActivity.this).load(imageURL).into(profileImg);
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.e("Upload error", e.getMessage());
-                            }
-                        });
+                    public void onSuccess(Uri uri) {
+                        String imageURL = uri.toString();
+                        FirebaseUser firebaseUser = auth.getCurrentUser();
+                        DatabaseReference userReference = FirebaseDatabase.getInstance().getReference().child("user").child(firebaseUser.getUid());
+                        userReference.child("userImg").setValue(imageURL);
+                        Glide.with(ProfileActivity.this).load(imageURL).into(profileImg);
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
@@ -180,12 +217,24 @@ public class ProfileActivity extends AppCompatActivity {
                         Log.e("Upload error", e.getMessage());
                     }
                 });
-
-            } else if(resultCode == RESULT_CANCELED) {
-                // User cancelled the action
             }
-        }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("Upload error", e.getMessage());
+            }
+        });
     }
+
+
+
+    public Uri getImageUri(Context context, Bitmap bitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap, "ProfilePic", null);
+        return Uri.parse(path);
+    }
+
     public void logout() {
         auth.signOut();  // sign out from firebase
         Intent intent = new Intent(ProfileActivity.this, homePage.class);
